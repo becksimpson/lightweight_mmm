Solved no valid initial parameters bug caused by jax invalid gradients

lightweight_mmm/media_transforms.py
@@ -117,9 +117,13 @@ def logistic_saturation(
     data: Input data.
     saturation: Controls the saturation, higher slope, stronger saturation
   """
+
   # TODO: Used with scaled beta
   saturation = 1.0 + saturation * 3.0
   log_data = (1.0 - jnp.exp(-saturation * data)) / (1.0 + jnp.exp(-saturation * data))
+  
+  return log_data
+  
   one_norm = (1.0 - jnp.exp(-saturation)) / (1.0 + jnp.exp(-saturation))
   
   return jax.lax.cond(
@@ -154,9 +158,16 @@ def hill_constrained(data: jnp.ndarray,
   # Range 0.5 --> 3.0
   slope = slope_constrained * 2.5 + 0.5
 
+  exponent_safe = (jnp.clip(data, a_min=1e-6) / half_max_effective_concentration) ** (-slope)
+  hill_media = 1. / (1 + exponent_safe)
+  return jnp.where(condition=(data==0.), x=0., y=hill_media)
+
   save_transform = apply_exponent_safe(
       data=data / half_max_effective_concentration, exponent=-slope)
-  hill_media = jnp.where(save_transform == 0, x=0, y=1. / (1 + save_transform))
+  
+  hill_media = jnp.where(data == 0, x=0, y=1. / (1. + save_transform)) 
+
+  return hill_media
 
   return jax.lax.cond(
     hill_normalise,
@@ -295,7 +306,10 @@ def apply_exponent_safe(
 
   More info on the double jnp.where can be found:
   https://github.com/tensorflow/probability/blob/main/discussion/where-nan.pdf
-
+  tf.where(x_ok, f(tf.where(x_ok, x, safe_x), safe_f(x))
+  Instead of:
+  tf.where(x_ok, f(x), safe_f(x))
+  
   Args:
     data: Input data to use.
     exponent: Exponent required for the operations.
@@ -303,5 +317,8 @@ def apply_exponent_safe(
   Returns:
     The result of the exponent operation with the inputs provided.
   """
-  exponent_safe = jnp.where(condition=(data == 0), x=1, y=data) ** exponent
-  return jnp.where(condition=(data == 0), x=0, y=exponent_safe)
+  # exponent_safe = jnp.where(condition=(data == 0), x=1, y=data) ** exponent
+  # return jnp.where(condition=(data == 0), x=0, y=exponent_safe)
+
+  exponent_safe = jnp.where(condition=(data != 0), x=data, y=1) ** exponent
+  return jnp.where(condition=(data != 0), x=exponent_safe, y=0)

lightweight_mmm/models.py
@@ -131,7 +131,7 @@ def _get_default_priors() -> Mapping[str, Prior]:
       _WEEKDAY: dist.TruncatedNormal(loc=0., scale=.1),
       #_COEF_EXTRA_FEATURES: dist.Normal(loc=0., scale=.1),
       #_COEF_EXTRA_FEATURES: dist.HalfNormal(scale=.2), 
-      _COEF_EXTRA_FEATURES: dist.Normal(loc=0., scale=.1),
+      _COEF_EXTRA_FEATURES: dist.Normal(loc=0., scale=.05),
       _COEF_SEASONALITY: dist.HalfNormal(scale=.5),
       _PARAM_DAY_OF_MONTH: dist.TruncatedNormal(loc=1.0, scale=0.5, low=1e-6),# dist.TruncatedNormal(loc=1.0, scale=0.5, low=1e-6),# low=0.1, high=10.0),
       _MULTIPLIER_DAY_OF_MONTH: dist.HalfNormal(scale=0.1),
@@ -1260,6 +1260,11 @@ def calculate_media_effects(
   if transform_function in carryover_models and 'number_lags' not in transform_kwargs:
     transform_kwargs['number_lags'] = 13 if frequency == 52 else 180# 120
 
+  # media_transformed = jnp.repeat(
+  #   jnp.expand_dims(media_data, 0),
+  #   repeats=4,
+  #   axis=0
+  # )
   media_transformed = apply_media_transform_function(
     transform_function,
     media_data,
@@ -1272,6 +1277,7 @@ def calculate_media_effects(
     name="media_transformed",
     value=media_transformed
   )
+
   if transform_function.__name__ not in [transform_ensemble_multi.__name__]:
 
     with numpyro.plate(
@@ -1418,6 +1424,7 @@ def calculate_media_effects(
         extra_features_effects
       )
     )
+    
 
     logger.debug(f'Channels: {channel_contribution.shape} {channel_contribution.mean(axis=0)}')
     logger.debug(f'Model Sigma: {model_sigma}')
@@ -1438,6 +1445,7 @@ def calculate_media_effects(
       )) if target_data is not None else None
     )
 
+    
     media_effects = numpyro.deterministic(
       name = 'media_contribution',
       value = channel_contribution.sum(axis=-1)

lightweight_mmm/plot.py
@@ -1731,7 +1731,6 @@ def plot_prior_and_posterior(
           #   hyp: media_mix_model.trace[feature+'_'+hyp].mean()
           #   for hyp in feature_hyperpriors
           # }
-          print(f'Mean Hyperpriors for {feature} is {mean_hyperprior_posterior_samples}')
           prior_distributions = [
             prior_cls(
               **mean_hyperprior_posterior_samples
@@ -1821,7 +1820,6 @@ def plot_prior_and_posterior(
               dd[p] = v
         if dd:
           if 'low' in dd and prior_distribution.__class__ in [numpyro.distributions.HalfNormal, numpyro.distributions.Normal]:
-            print(dd)
             prior_distribution = numpyro.distributions.TruncatedNormal(**dd)
           else:
             prior_distribution = prior_distribution.__class__(**dd)
@@ -1847,7 +1845,10 @@ def plot_prior_and_posterior(
     if feature in geo_level_features:
       for i_geo in range(media_mix_model.n_geos):
         subplot_title = f"{feature}, geo {i_geo}"
-        posterior_samples = np.array(media_mix_model.trace[feature][:, i_geo])
+        if len(media_mix_model.trace[feature].shape) > 1:
+          posterior_samples = np.array(media_mix_model.trace[feature][:, i_geo])
+        else:
+          posterior_samples = np.array(media_mix_model.trace[feature])
         (fig, gridspec_fig,
           i_ax) = _make_prior_and_posterior_subplot_for_one_feature(
               posterior_samples=posterior_samples,
@@ -1887,7 +1888,6 @@ def plot_prior_and_posterior(
         if feature == 'coef_media_models':
           posterior_samples = np.array(
               media_mix_model.trace[feature][:, :, i_channel])
-          print(posterior_samples.shape)
           posterior_names = [f'model_{i}' for i in range(posterior_samples.shape[0])]
         else:
           posterior_samples = np.array(